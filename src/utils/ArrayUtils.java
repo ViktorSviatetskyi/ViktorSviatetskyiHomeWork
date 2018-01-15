847ba778847d871b00752ecc6906fedbb813d275
ViktorSviatetskyi <avtobussnik@gmail.com>
2018-01-16 00:19:07 +0200

    ArrayUtils

--- src/utils/ArrayUtils.java
+++ src/utils/ArrayUtils.java
@@ -3,15 +3,12 @@ package utils;
 public class ArrayUtils {
 
     public static int [] createAndFillArray (int size) {
-
         int[] array = new int[size];
-
         for (int i = 0; i < array.length; i++) {
           array[i] = i;
         }
     return array;
     }
-
     public static void printArray (int[] array) {
         for (int i = 0; i< array.length; i++){
             System.out.print(array[i] + " ");
@@ -22,7 +19,6 @@ public class ArrayUtils {
             System.out.print(array[i] + " ");
         }
     }
-
     public static int [] createAndFillRandomArray (int size) {
         int[] array = new int[size];
 
@@ -31,4 +27,37 @@ public class ArrayUtils {
         }
     return array;
     }
+
+    public static int findMin (int [] array){
+        int [] mas = array;
+        int min = mas[0];
+
+        for (int i = 0; i < mas.length; i++) {
+            if (mas[i]< min){
+                min = mas[i];
+            }
+        }
+        return min;
+    }
+    public static int findMax (int [] array){
+        int [] mas = array;
+        int max = mas[0];
+
+        for (int i = 0; i < mas.length; i++) {
+            if (mas[i]> max){
+                max = mas[i];
+            }
+        }
+        return max;
+    }
+    public static double arrAverage (int [] arrey){
+        int [] mas = arrey;
+        double sum=0;
+        for (int i = 0; i< mas.length; i++){
+            sum = sum + mas[i];
+        }
+        double average = sum/mas.length;
+        return average;
+    }
 }
+
