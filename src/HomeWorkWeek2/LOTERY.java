df143588504f5cc514dcc9c8be83a7a88e9d3b82
ViktorSviatetskyi <avtobussnik@gmail.com>
2018-01-20 00:49:10 +0200

    ArrayUtils

--- src/HomeWorkWeek2/LOTERY.java
+++ src/HomeWorkWeek2/LOTERY.java
@@ -6,8 +6,7 @@ import utils.ArrayUtils;
 
 public class LOTERY {
     public static void main(String[] args) {
-        ArrayUtils.enterSixNum();
-        int[] randArr = ArrayUtils.genSixRandNum();
+         int[] randArr = ArrayUtils.genSixRandNum();
         randArr = ArrayUtils.sortArr(randArr);
         System.out.println("Крутиться барабан... і видає нам шість чисел");
         ArrayUtils.printArray(randArr);
